da8033388cf322020935d60d31fd023bac6af65a
roofimon <roofimon@f7ead51a-5a89-11de-bbaa-f1a44a2b9d47>
2009-07-07 08:32:12 +0000

    First Release for Service Test

--- branches/pre/src/test/java/org/springframework/petclinic/jpa/AbstractJpaClinicTests.java
+++ branches/pre/src/test/java/org/springframework/petclinic/jpa/AbstractJpaClinicTests.java
@@ -17,13 +17,13 @@ package org.springframework.petclinic.jpa;
 
 import static org.junit.Assert.assertEquals;
 import com.spring66.petclinic.entity.Owner;
+import com.spring66.petclinic.entity.Vet;
 import com.spring66.petclinic.service.Clinic;
 import java.util.Collection;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.AbstractTransactionalJUnit4SpringContextTests;
-
 
 /**
  * <p>
@@ -66,7 +66,6 @@ public abstract class AbstractJpaClinicTests extends AbstractTransactionalJUnit4
     @Autowired
     protected Clinic clinic;
 
-
     /**
      * This method is provided to set the Clinic instance being tested by the
      * Dependency Injection injection behaviour of the superclass from the
@@ -77,60 +76,63 @@ public abstract class AbstractJpaClinicTests extends AbstractTransactionalJUnit4
     public void setClinic(Clinic clinic) {
         this.clinic = clinic;
     }
-    /*@Test
+
+    @Test
     public void testGetVets() {
-    Collection<Vet> vets = this.clinic.getVets();
-    // Use the inherited countRowsInTable() convenience method (from
-    // AbstractTransactionalDataSourceSpringContextTests) to verify the
-    // results.
-    assertEquals("JDBC query must show the same number of vets", super.countRowsInTable("VETS"), vets.size());
-    Vet v1 = EntityUtils.getById(vets, Vet.class, 2);
-    assertEquals("Leary", v1.getLastName());
-    assertEquals(1, v1.getNrOfSpecialties());
-    assertEquals("radiology", (v1.getSpecialties().get(0)).getName());
-    Vet v2 = EntityUtils.getById(vets, Vet.class, 3);
-    assertEquals("Douglas", v2.getLastName());
-    assertEquals(2, v2.getNrOfSpecialties());
-    assertEquals("dentistry", (v2.getSpecialties().get(0)).getName());
-    assertEquals("surgery", (v2.getSpecialties().get(1)).getName());
+        Collection<Vet> vets = this.clinic.getVets();
+        // Use the inherited countRowsInTable() convenience method (from
+        // AbstractTransactionalDataSourceSpringContextTests) to verify the
+        // results.
+        assertEquals("JDBC query must show the same number of vets", super.countRowsInTable("VETS"), vets.size());
+        Vet v1 = EntityUtils.getById(vets, Vet.class, 2);
+        assertEquals("Leary", v1.getLastName());
+        assertEquals(1, v1.getNrOfSpecialties());
+        assertEquals("radiology", (v1.getSpecialties().get(0)).getName());
+        Vet v2 = EntityUtils.getById(vets, Vet.class, 3);
+        assertEquals("Douglas", v2.getLastName());
+        assertEquals(2, v2.getNrOfSpecialties());
+        assertEquals("dentistry", (v2.getSpecialties().get(0)).getName());
+        assertEquals("surgery", (v2.getSpecialties().get(1)).getName());
     }
 
     @Test
     public void testGetPetTypes() {
-    Collection<PetType> petTypes = this.clinic.getPetTypes();
-    assertEquals("JDBC query must show the same number of pet types", super.countRowsInTable("TYPES"),
-    petTypes.size());
-    PetType t1 = EntityUtils.getById(petTypes, PetType.class, 1);
-    assertEquals("cat", t1.getName());
-    PetType t4 = EntityUtils.getById(petTypes, PetType.class, 4);
-    assertEquals("snake", t4.getName());
+        Collection<PetType> petTypes = this.clinic.getPetTypes();
+        assertEquals("JDBC query must show the same number of pet types", super.countRowsInTable("TYPES"),
+                petTypes.size());
+        PetType t1 = EntityUtils.getById(petTypes, PetType.class, 1);
+        assertEquals("cat", t1.getName());
+        PetType t4 = EntityUtils.getById(petTypes, PetType.class, 4);
+        assertEquals("snake", t4.getName());
     }
 
     @Test
     public void testFindOwners() {
-    Collection<Owner> owners = this.clinic.findOwners("Davis");
-    assertEquals(2, owners.size());
-    owners = this.clinic.findOwners("Daviss");
-    assertEquals(0, owners.size());
+        Collection<Owner> owners = this.clinic.findOwners("Davis");
+        assertEquals(2, owners.size());
+        owners = this.clinic.findOwners("Daviss");
+        assertEquals(0, owners.size());
     }
 
     @Test
     public void testLoadOwner() {
-    Owner o1 = this.clinic.loadOwner(1);
-    assertTrue(o1.getLastName().startsWith("Franklin"));
-    Owner o10 = this.clinic.loadOwner(10);
-    assertEquals("Carlos", o10.getFirstName());
+        Owner o1 = this.clinic.loadOwner(1);
+        assertTrue(o1.getLastName().startsWith("Franklin"));
+        Owner o10 = this.clinic.loadOwner(10);
+        assertEquals("Carlos", o10.getFirstName());
 
-    // Check lazy loading, by ending the transaction
-    // endTransaction();
+        // Check lazy loading, by ending the transaction
+        // endTransaction();
 
-    // Now Owners are "disconnected" from the data store.
-    // We might need to touch this collection if we switched to lazy loading
-    // in mapping files, but this test would pick this up.
-    o1.getPets();
-    }*/
+        // Now Owners are "disconnected" from the data store.
+        // We might need to touch this collection if we switched to lazy loading
+        // in mapping files, but this test would pick this up.
+        o1.getPets();
+    }
+    
+
     @Test
-    public void testSomething(){
+    public void testSomething() {
         assertEquals("xxx", "x");
     }
 
@@ -145,67 +147,67 @@ public abstract class AbstractJpaClinicTests extends AbstractTransactionalJUnit4
         owners = this.clinic.findOwners("Schultz");
         assertEquals(found + 1, owners.size());
     }
-    /*@Test
+
+    @Test
     public void testUpdateOwner() throws Exception {
-    Owner o1 = this.clinic.loadOwner(1);
-    String old = o1.getLastName();
-    o1.setLastName(old + "X");
-    this.clinic.storeOwner(o1);
-    o1 = this.clinic.loadOwner(1);
-    assertEquals(old + "X", o1.getLastName());
+        Owner o1 = this.clinic.loadOwner(1);
+        String old = o1.getLastName();
+        o1.setLastName(old + "X");
+        this.clinic.storeOwner(o1);
+        o1 = this.clinic.loadOwner(1);
+        assertEquals(old + "X", o1.getLastName());
     }
 
     @Test
     public void testLoadPet() {
-    Collection<PetType> types = this.clinic.getPetTypes();
-    Pet p7 = this.clinic.loadPet(7);
-    assertTrue(p7.getName().startsWith("Samantha"));
-    assertEquals(EntityUtils.getById(types, PetType.class, 1).getId(), p7.getType().getId());
-    assertEquals("Jean", p7.getOwner().getFirstName());
-    Pet p6 = this.clinic.loadPet(6);
-    assertEquals("George", p6.getName());
-    assertEquals(EntityUtils.getById(types, PetType.class, 4).getId(), p6.getType().getId());
-    assertEquals("Peter", p6.getOwner().getFirstName());
+        Collection<PetType> types = this.clinic.getPetTypes();
+        Pet p7 = this.clinic.loadPet(7);
+        assertTrue(p7.getName().startsWith("Samantha"));
+        assertEquals(EntityUtils.getById(types, PetType.class, 1).getId(), p7.getType().getId());
+        assertEquals("Jean", p7.getOwner().getFirstName());
+        Pet p6 = this.clinic.loadPet(6);
+        assertEquals("George", p6.getName());
+        assertEquals(EntityUtils.getById(types, PetType.class, 4).getId(), p6.getType().getId());
+        assertEquals("Peter", p6.getOwner().getFirstName());
     }
 
     @Test
     public void testInsertPet() {
-    Owner o6 = this.clinic.loadOwner(6);
-    int found = o6.getPets().size();
-    Pet pet = new Pet();
-    pet.setName("bowser");
-    Collection<PetType> types = this.clinic.getPetTypes();
-    pet.setType(EntityUtils.getById(types, PetType.class, 2));
-    pet.setBirthDate(new Date());
-    o6.addPet(pet);
-    assertEquals(found + 1, o6.getPets().size());
-    this.clinic.storeOwner(o6);
-    // assertTrue(!pet.isNew()); -- NOT TRUE FOR TOPLINK (before commit)
-    o6 = this.clinic.loadOwner(6);
-    assertEquals(found + 1, o6.getPets().size());
+        Owner o6 = this.clinic.loadOwner(6);
+        int found = o6.getPets().size();
+        Pet pet = new Pet();
+        pet.setName("bowser");
+        Collection<PetType> types = this.clinic.getPetTypes();
+        pet.setType(EntityUtils.getById(types, PetType.class, 2));
+        pet.setBirthDate(new Date());
+        o6.addPet(pet);
+        assertEquals(found + 1, o6.getPets().size());
+        this.clinic.storeOwner(o6);
+        // assertTrue(!pet.isNew()); -- NOT TRUE FOR TOPLINK (before commit)
+        o6 = this.clinic.loadOwner(6);
+        assertEquals(found + 1, o6.getPets().size());
     }
 
     @Test
     public void testUpdatePet() throws Exception {
-    Pet p7 = this.clinic.loadPet(7);
-    String old = p7.getName();
-    p7.setName(old + "X");
-    this.clinic.storePet(p7);
-    p7 = this.clinic.loadPet(7);
-    assertEquals(old + "X", p7.getName());
+        Pet p7 = this.clinic.loadPet(7);
+        String old = p7.getName();
+        p7.setName(old + "X");
+        this.clinic.storePet(p7);
+        p7 = this.clinic.loadPet(7);
+        assertEquals(old + "X", p7.getName());
     }
 
     @Test
     public void testInsertVisit() {
-    Pet p7 = this.clinic.loadPet(7);
-    int found = p7.getVisits().size();
-    Visit visit = new Visit();
-    p7.addVisit(visit);
-    visit.setDescription("test");
-    this.clinic.storePet(p7);
-    // assertTrue(!visit.isNew()); -- NOT TRUE FOR TOPLINK (before commit)
-    p7 = this.clinic.loadPet(7);
-    assertEquals(found + 1, p7.getVisits().size());
-    }*/
-
+        Pet p7 = this.clinic.loadPet(7);
+        int found = p7.getVisits().size();
+        Visit visit = new Visit();
+        p7.addVisit(visit);
+        visit.setDescription("test");
+        this.clinic.storePet(p7);
+        // assertTrue(!visit.isNew()); -- NOT TRUE FOR TOPLINK (before commit)
+        p7 = this.clinic.loadPet(7);
+        assertEquals(found + 1, p7.getVisits().size());
+    }
 }
